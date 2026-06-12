9a41f7a96c652e08a7dcc79fb3275daa2863a57b
Hemnath Balasubramanian <hemnath@thepenindia.com>
2026-06-12 11:46:22 +0530

    fixed cors config wrong annotation

--- src/main/java/com/example/practicespringboot/Practice/Springboot/config/CorsConfig.java
+++ src/main/java/com/example/practicespringboot/Practice/Springboot/config/CorsConfig.java
@@ -1,15 +1,19 @@
 package com.example.practicespringboot.Practice.Springboot.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
